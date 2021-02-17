847a20f90dea49760ced101c5366c245ff2a8963
Isaac King <idrkings@gmail.com>
2021-02-16 16:10:59 -0800

    Add Confirm component to Appointment

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 
 storiesOf("Button", module)
@@ -147,4 +148,13 @@ storiesOf("Button", module)
           interviewer={interviewer}
           onEdit={action("onEdit")}
           onDelete={action("onDelete")}
-        />)});
+        />)})
+        .add("Confirm", () => {
+          return (
+            <Confirm 
+              message="Delete the appointment?"
+              onConfirm={action("onConfirm")}
+              onCancel={action("onCancel")}
+            />
+          );
+        })

85e0f59a9ab2010e2351b7a5927e97857ea9af47
Isaac King <idrkings@gmail.com>
2021-02-19 23:03:22 -0800

    Fixed spot calculator

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -14,10 +14,7 @@ const useApplicationData = () => {
       case SET_DAY:
         return {...state, day: action.day};
       case SET_APPLICATION_DATA:
-        console.log('ACTION----', action);
-        console.log('appointments? ', action.appointments)
         state = {...state, days: action.days, appointments: action.appointments, interviewers: action.interviewers};
-        console.log('STATE ---', state);
         return state;
       case SET_INTERVIEW:
         return {...state, days: action.days, appointments: action.appointments}
@@ -26,9 +23,6 @@ const useApplicationData = () => {
       }
     }
     
-
-
-
   const setDay = day =>  dispatch({type: SET_DAY, day})
 
   useEffect(() => {
@@ -41,40 +35,26 @@ const useApplicationData = () => {
           type: SET_APPLICATION_DATA,
           days: all[0].data,
           appointments: all[1].data,
-          interviewers: all[2].data });
-      // setState(prev => ({
-      //   ...prev,
-      //   days: all[0].data,
-      //   appointments: all[1].data,
-      //   interviewers: all[2].data
-      // }));
-
+          interviewers: all[2].data 
+        });
     })
   }, []);
   
-
-
-
+  
+  
   const manageInterview = (id, interview = null) => {
     const appointment = {...state.appointments[id], interview: interview ? {...interview} : null}
     const appointments = {...state.appointments, [id]: appointment}
     const dayIndex = getDayIndexByAppointment(state, id);
     const days = [...state.days];
-    days[dayIndex].spots += interview ? -1 : 1;
+    const spotReducer = (acc, id) =>  appointments[id].interview ? acc : acc + 1;
+    days[dayIndex].spots = days[dayIndex].appointments.reduce(spotReducer, 0)
 
     const request = interview ? axios.put : axios.delete;
     return request(`/api/appointments/${id}`, appointment)
       .then(() => {
         dispatch({type: SET_INTERVIEW, appointments, days})
       }
-      
-      // setState(prev => {
-      //   return {
-      //     ...prev,
-      //     appointments,
-      //     days
-      //   }
-      // })
     )
   };
 
